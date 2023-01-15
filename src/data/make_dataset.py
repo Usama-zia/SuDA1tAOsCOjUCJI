6bb1164fb319d14a83c4f3cad8d27bd7f6ce41e3
Usama Muhammadi <musamazia736@gmail.com>
2023-01-15 23:05:46 +0000

    update

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
+"""Load the dataset and provide some basic info."""
 import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import numpy as np
+import pandas as pd
 
 
 @click.command()
@@ -28,3 +30,24 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
 
     main()
+
+
+
+class dataset():
+    def load():
+        dataset = pd.read_excel('../data/raw/2020Q1Q2Q3Q4-2021Q1.xlsx', sheet_name = None,
+                                parse_dates=['Date'], index_col = 'Date', skipfooter=1)
+
+        #check how many sheets are in the file
+        print('Sheets in dataset: ',len(dataset.keys()))
+
+        #Some basic info for every sheet in dataset.
+        for sheet in range(len(dataset.keys())):
+            each_set = dataset[list(dataset.keys())[sheet]]
+            print(f'----------Name of dataset is "{list(dataset.keys())[sheet]}"----------')
+            print(each_set.head())
+            print(f'Null values in dataset are \n{each_set.isnull().sum()}')
+            print(f'Duplicated values in dataset are {each_set.duplicated().sum()}')
+            print('\n\n\n')
+
+        return dataset
